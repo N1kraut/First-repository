ee6e3bafe9d5296b50394d22972d6137a412fee5
N1kraut <N.Pecherkin@mail.ru>
2020-04-01 20:47:28 +0300

    'laba_2_oib

---
+++ xor.py
@@ -0,0 +1,17 @@
+
+
+
+def xor(a,b):
+    """
+    xorир по байтам
+
+    >>> xor('000100010000', '10010111‬0001')
+    100001100001
+    """
+    return 100001100001
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
+
