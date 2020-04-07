697b11baa7f02af3394bed17125d865328366cfc
N1kraut <N.Pecherkin@mail.ru>
2020-04-07 18:40:07 +0300

    laba_3_cipher_cezar

---
+++ Cezar.py
@@ -0,0 +1,19 @@
+
+def qwe(a,b):
+
+    """
+
+    >>> qwe('Nikita', 'Pecherkin')
+    'xdhaoi'
+
+    """
+    c = 'xdhaoi'
+    return c
+
+
+
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
--- xor.py
+++ xor.py
@@ -11,9 +11,18 @@ def xor(a,b):
     ia = int(a)
     ib = int(b)
     return ia^ib
+"""
+def hexxor(a,b):
 
+    xorрит хексовые
+
+    >>>hexxor(110, 97)
+    йцу
+
+    qwe = hexlify(bytes(c1 ^ c2 for c1, c2 in zip(unhexlify(a), unhexlify(b))))
+    return qwe
+"""
 
 if __name__ == "__main__":
     import doctest
     doctest.testmod(verbose=True)
-
